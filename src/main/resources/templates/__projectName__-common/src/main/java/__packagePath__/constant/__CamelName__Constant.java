dd40b7047c61fca89fdb82ac15f3926d388c88a5
coder4 <lihy@fenbi.com>
2017-12-05 11:35:20 +0800

    ADD: Constant

---
+++ src/main/resources/templates/__projectName__-common/src/main/java/__packagePath__/constant/__CamelName__Constant.java
@@ -0,0 +1,12 @@
+package @packageName@.constant;
+
+/**
+ * @author coder4
+ */
+public class @CamelName@Constant {
+
+    public static final String PROJECT_NAME = "@name@";
+
+    public static final String REST_API = "/@name@/{client:api|iphone|ipad|android|win|small}";
+
+}
